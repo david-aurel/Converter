253687c7bf502dc4c55bbf98b4247d9c46838ff0
David <53895961+david-aurel@users.noreply.github.com>
2019-10-31 12:44:55 +0100

    add celsius & fahrenheit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // select everything
-let kmInput     = document.querySelector("#km"),
-    milesInput  = document.querySelector("#miles"),
-    literInput  = document.querySelector("#liter"),
-    cupsInput   = document.querySelector("#cups");
+let kmInput         = document.querySelector("#km"),
+    milesInput      = document.querySelector("#miles"),
+    literInput      = document.querySelector("#liter"),
+    cupsInput       = document.querySelector("#cups"),
+    celsiusInput    = document.querySelector("#celsius"),
+    fahrenheitInput = document.querySelector("#fahrenheit");
 
 
 // event listeners
@@ -22,6 +24,14 @@ cupsInput.addEventListener('keyup', function(value) {
     cupsToLiters(cupsInput.value);
 });
 
+celsiusInput.addEventListener('keyup', function(value) {
+    celsiusToFahrenheit(celsiusInput.value);
+});
+
+fahrenheitInput.addEventListener('keyup', function(value) {
+    fahrenheitToCelsius(fahrenheitInput.value);
+});
+
 
 // km to miles
 function kmToMiles(km) {
@@ -39,4 +49,12 @@ function literToCups(liter) {
 function cupsToLiters(cups) {
     literInput.value = (cups / 4.227).toFixed(2);
 };
+// celsius to fahrenheit
+function celsiusToFahrenheit(celsius) {
+    fahrenheitInput.value = ((celsius * (9/5)) + 32).toFixed(0);
+};
+// fahrenheit to celsius
+function fahrenheitToCelsius(fahrenheit) {
+    celsiusInput.value = ((fahrenheit - 32) * (5/9)).toFixed(1);
+}; 
 
