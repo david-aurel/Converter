08396bb58e95b124c417403522956a6361e48e66
David <53895961+david-aurel@users.noreply.github.com>
2019-10-31 11:39:23 +0100

    add km & miles

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+// select everything
+let kmInput = document.querySelector("#km"),
+    milesInput = document.querySelector("#miles");
+
+
+// event listeners
+kmInput.addEventListener('keyup', function(value) {
+    kmToMiles(kmInput.value);
+});
+
+milesInput.addEventListener('keyup', function(value) {
+    milesToKm(milesInput.value);
+});
+
+
 // km to miles
 function kmToMiles(km) {
-    return (km / 1.609).toFixed(2);
+    milesInput.value = (km / 1.609).toFixed(2);
 }
-
-console.log(kmToMiles(5))
+// miles to km
+function milesToKm(miles) {
+    kmInput.value = (miles * 1.609).toFixed(2);
+}
