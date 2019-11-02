80d2b962ece2a656ceeee431f82911dd9300a227
David <53895961+david-aurel@users.noreply.github.com>
2019-11-02 08:17:27 +0100

    refactor eventlisteners

--- app.js
+++ app.js
@@ -6,73 +6,57 @@ let kmInput         = document.querySelector("#km"),
     celsiusInput    = document.querySelector("#celsius"),
     fahrenheitInput = document.querySelector("#fahrenheit"),
     euroInput       = document.querySelector("#euro"),
-    dollarInput     = document.querySelector("#dollar");
+    dollarInput     = document.querySelector("#dollar")
 
 
-// event listeners
-kmInput.addEventListener('keyup', function(value) {
-    kmToMiles(kmInput.value);
-});
-
-milesInput.addEventListener('keyup', function(value) {
-    milesToKm(milesInput.value);
-});
-
-literInput.addEventListener('keyup', function(value) {
-    literToCups(literInput.value);
-});
-
-cupsInput.addEventListener('keyup', function(value) {
-    cupsToLiters(cupsInput.value);
-});
-
-celsiusInput.addEventListener('keyup', function(value) {
-    celsiusToFahrenheit(celsiusInput.value);
-});
-
-fahrenheitInput.addEventListener('keyup', function(value) {
-    fahrenheitToCelsius(fahrenheitInput.value);
-});
-
-euroInput.addEventListener('keyup', function(value) {
-    euroToDollar(euroInput.value);
-});
-
-dollarInput.addEventListener('keyup', function(value) {
-    dollarToEuro(dollarInput.value);
-});
+// arrays to be used by the eventListener function
+let selector = [kmInput, milesInput, literInput, cupsInput, celsiusInput, fahrenheitInput, euroInput, dollarInput]
+let calculation = [kmToMiles, milesToKm, literToCups, cupsToLiters, celsiusToFahrenheit, fahrenheitToCelsius, euroToDollar, dollarToEuro]
 
 
-// km to miles
+//functions for the calculations of the results
 function kmToMiles(km) {
-    milesInput.value = (km / 1.609).toFixed(2);
-};
-// miles to km
+    milesInput.value = (km / 1.609).toFixed(2)
+}
 function milesToKm(miles) {
-    kmInput.value = (miles * 1.609).toFixed(2);
-};
-// liter to cups
+    kmInput.value = (miles * 1.609).toFixed(2)
+}
 function literToCups(liter) {
-    cupsInput.value = (liter * 4.227).toFixed(2);
-};
-// cups to liter
+    cupsInput.value = (liter * 4.227).toFixed(2)
+}
 function cupsToLiters(cups) {
-    literInput.value = (cups / 4.227).toFixed(2);
-};
-// celsius to fahrenheit
+    literInput.value = (cups / 4.227).toFixed(2)
+}
 function celsiusToFahrenheit(celsius) {
-    fahrenheitInput.value = ((celsius * (9/5)) + 32).toFixed(0);
-};
-// fahrenheit to celsius
+    fahrenheitInput.value = ((celsius * (9/5)) + 32).toFixed(0)
+}
 function fahrenheitToCelsius(fahrenheit) {
-    celsiusInput.value = ((fahrenheit - 32) * (5/9)).toFixed(1);
-}; 
-// euro to dollar
+    celsiusInput.value = ((fahrenheit - 32) * (5/9)).toFixed(1)
+} 
 function euroToDollar(euro) {
-    dollarInput.value = (euro * 1.11504).toFixed(2);
-};
-// dollar to euro
+    dollarInput.value = (euro * 1.11504).toFixed(2)
+}
 function dollarToEuro(dollar) {
-    euroInput.value = (dollar * 0.89683).toFixed(2);
-}; 
+    euroInput.value = (dollar * 0.89683).toFixed(2)
+} 
+
+
+// function to make eventlistener
+function makeEventListener(input, convertFunction) {
+    input.addEventListener("keyup", function(value) {
+        convertFunction(input.value)
+    })    
+}    
+
+// function pass in the right arguments into the eventListener function
+function makeEventlistenerArguments(input, calculation) {
+    for (let i = 0; i < selector.length; i++) {
+        makeEventListener(input[i], calculation[i])
+    }    
+}    
+
+//call function to pass in arguments to make eventListener
+makeEventlistenerArguments(selector, calculation)
+
+
 
