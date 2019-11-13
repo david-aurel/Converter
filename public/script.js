04d85fb38b0014d67fd0ec9879347dd32b2c7606
David <53895961+david-aurel@users.noreply.github.com>
2019-11-13 18:35:57 +0100

    refactor a bunch

--- public/script.js
+++ public/script.js
@@ -4,40 +4,19 @@ let dropdown           = document.getElementById("dropdown"),
     output             = document.getElementById("output"),
     outputDisplay      = document.getElementById("outputDisplay")
     
-
 //function for the calculations of the results
 function calculation(input, dropdown) {
-    //look at dropdown
     switch(dropdown) {
-        //make cases
-        case "km":
-            //do calculation for each case
-            output.value = (input / 1.609).toFixed(2)
-        break
-        case "miles":
-            return output.value = (input * 1.609).toFixed(2)
-        break
-        case "liter":
-            return  output.value = (input * 4.227).toFixed(2)
-        break
-        case "cups":
-            return output.value = (input / 4.227).toFixed(2)
-        break
-        case "C":
-            return output.value = ((input * (9/5)) + 32).toFixed(0)
-        break
-        case "F":
-            return output.value = ((input / (9/5)) + 32).toFixed(0)
-        break
-        case "euro":
-            return output.value = (input * 1.11504).toFixed(2)
-        break
-        case "dollar":
-            return output.value = (input / 1.11504).toFixed(2)
-        break
+        case "km":          return output.value = (input / 1.609).toFixed(2);           break
+        case "miles":       return output.value = (input * 1.609).toFixed(2);           break
+        case "liter":       return output.value = (input * 4.227).toFixed(2);           break
+        case "cups":        return output.value = (input / 4.227).toFixed(2);           break
+        case "C":           return output.value = ((input * (9/5)) + 32).toFixed(0);    break
+        case "F":           return output.value = ((input / (9/5)) + 32).toFixed(0);    break
+        case "euro":        return output.value = (input * 1.11504).toFixed(2);         break
+        case "dollar":      return output.value = (input / 1.11504).toFixed(2);         break
     }
 }
-
 
 //function for setting the correct unit
 function setUnit (dropdown) {
@@ -56,6 +35,3 @@ dropdown.addEventListener('change', function(){
     calculation(input.value, dropdown.value)
     setUnit(dropdown.value)
 })
-    
-
-
