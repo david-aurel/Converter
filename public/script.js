27ae47053012bb5458e4760ddfb46b1ad10ba7f2
David <53895961+david-aurel@users.noreply.github.com>
2019-11-13 18:27:26 +0100

    refactor setUnit function

--- public/script.js
+++ public/script.js
@@ -16,67 +16,46 @@ function calculation(input, dropdown) {
         break
         case "miles":
             return output.value = (input * 1.609).toFixed(2)
-            outputDisplay.value = "km"
         break
         case "liter":
             return  output.value = (input * 4.227).toFixed(2)
-            outputDisplay.value = "cups"
         break
         case "cups":
             return output.value = (input / 4.227).toFixed(2)
-                outputDisplay.value = "liter"
         break
         case "C":
             return output.value = ((input * (9/5)) + 32).toFixed(0)
-            outputDisplay.value = "˚F"
         break
         case "F":
             return output.value = ((input / (9/5)) + 32).toFixed(0)
-            outputDisplay.value = "˚C"
         break
         case "euro":
             return output.value = (input * 1.11504).toFixed(2)
-            outputDisplay.value = "dollar"
         break
         case "dollar":
             return output.value = (input / 1.11504).toFixed(2)
-            outputDisplay.value = "euro"
         break
     }
 }
 
-//function for setting the correct unit
+
+//refactored function for setting the correct unit
 function setUnit (dropdown) {
-    switch (dropdown) {
-        case "km":
-            //set right unit to outputDisplay
-            return outputDisplay.textContent = "miles"
-        case "miles":
-            return outputDisplay.textContent = "km"
-        case "liter":
-            return outputDisplay.textContent = "cups"
-        case "cups":
-            return outputDisplay.textContent = "liter"
-        case "C":
-            return outputDisplay.textContent = "˚F"
-        case "F":                                                                                                                                                                                                           
-            return outputDisplay.textContent = "˚C"
-        case "euro":
-            return outputDisplay.textContent = "$"
-        case "dollar":
-            return outputDisplay.textContent = "€"
-    }
+    let unitArr = ["km", "miles", "liter", "cups", "C", "F", "euro", "dollar"],
+        index   = unitArr.indexOf(dropdown)
+
+     return index % 2 === 0 ? outputDisplay.textContent = unitArr[index+1] : outputDisplay.textContent = unitArr[index-1]
 }
 
 // eventlistener
-    input.addEventListener("keyup", function() {
-        calculation(input.value, dropdown.value)
-    }) 
+input.addEventListener("keyup", function() {
+    calculation(input.value, dropdown.value)
+}) 
 
-    dropdown.addEventListener('change', function(){
-        calculation(input.value, dropdown.value)
-        setUnit(dropdown.value)
-    })
+dropdown.addEventListener('change', function(){
+    calculation(input.value, dropdown.value)
+    setUnit(dropdown.value)
+})
     
 
 
