572c5f872f488f985a5727316162bc7c13180dc9
David <53895961+david-aurel@users.noreply.github.com>
2019-11-14 00:48:15 +0100

    set up api to retrieve actual usd exchange rate

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+//require packages and set up variables
 const express   = require("express"),
+      request   = require('request'),
       app       = express(),
-      PORT      = process.env.PORT || 3000 
+      PORT      = process.env.PORT || 3000
 
+//use the 'public' directory
 app.use(express.static('public'))
 
+//home route
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
 })
 
+//listen on port
 app.listen(PORT, function(){
     console.log("Converter Server has started!");
 })
+
+// //API to get USD exchange rate
+// request('https://api.exchangeratesapi.io/latest', function (error, response, body) {
+//   let data = JSON.parse(body),
+//       usd = data['rates']['USD']
+//       console.log(usd)
+// })
+
--- public/script.js
+++ public/script.js
@@ -1,9 +1,17 @@
+//api for usd
+let usd = ''
+fetch('https://api.exchangeratesapi.io/latest')
+.then((res) => res.json())
+.then(function(data){ usd = data.rates.USD })
+
+
 // select everything
 let dropdown           = document.getElementById("dropdown"),
     input              = document.getElementById("input"),
     output             = document.getElementById("output"),
     outputDisplay      = document.getElementById("outputDisplay")
     
+
 //function for the calculations of the results
 function calculation(input, dropdown) {
     switch(dropdown) {
@@ -12,9 +20,9 @@ function calculation(input, dropdown) {
         case "liter":       return output.value = (input * 4.227).toFixed(2);           break
         case "cups":        return output.value = (input / 4.227).toFixed(2);           break
         case "˚C":          return output.value = ((input * (9/5)) + 32).toFixed(0);    break
-        case "˚F":          return output.value = ((input -32) * 5/9).toFixed(0);    break
-        case "€":           return output.value = (input * 1.101195).toFixed(2);         break
-        case "$":           return output.value = (input / 1.101195).toFixed(2);         break
+        case "˚F":          return output.value = ((input -32) * 5/9).toFixed(0);       break
+        case "€":           return output.value = (input * usd).toFixed(2);             break
+        case "$":           return output.value = (input / usd).toFixed(2);             break
     }
 }
 
