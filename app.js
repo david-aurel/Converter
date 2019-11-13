06e275ac6a0fe9c8276d9479ba5031ccec2133a8
David <53895961+david-aurel@users.noreply.github.com>
2019-11-13 13:12:49 +0100

    redo all the server stuff and link to everything correctly

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express   = require("express"),
       app       = express();
 
 
-app.use(express.static('__dirname + /public'))
-app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
-    res.render('index')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000, function(){
