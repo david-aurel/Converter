50230015b086aa8de79b43e147d4fb0fc6394579
David <53895961+david-aurel@users.noreply.github.com>
2019-11-14 01:03:28 +0100

    remove api commetn

--- app.js
+++ app.js
@@ -17,10 +17,3 @@ app.listen(PORT, function(){
     console.log("Converter Server has started!");
 })
 
-// //API to get USD exchange rate
-// request('https://api.exchangeratesapi.io/latest', function (error, response, body) {
-//   let data = JSON.parse(body),
-//       usd = data['rates']['USD']
-//       console.log(usd)
-// })
-
